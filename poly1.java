9263a73875ceae2c55c9a0cf8ac6a269f34d8777
safalnamdeo <safalnamdeo@gmail.com>
2026-02-26 13:58:23 +0530

    Add files via upload

---
+++ poly1.java
@@ -0,0 +1,26 @@
+//  class poly1 {
+//     void add(){
+//         System.out.println("hello");
+//     }
+//     void add(int a){
+
+//     }
+//     void add(int a, int b){
+//         System.out.println(a+b);
+//     }
+
+//     public static void main(String[] args) {
+//         poly1 s1=new poly1();
+//         s1.add(5,6);
+//     }
+// }
+
+public class poly1 {
+
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int sum= a+b;
+        System.out.println("Sum =" + sum);
+    }
+}
---
+++ poly2.java
@@ -0,0 +1,21 @@
+ class Animal {
+    void sound(){
+        System.out.println("animal");
+    }
+    
+}
+class Dog extends Animal{
+    void sound(){
+        System.out.println("dog");
+    }
+}
+class cat extends Animal{
+    void sound(){
+        System.out.println("cat");
+    }
+}
+
+ class poly2 {
+
+    
+}
